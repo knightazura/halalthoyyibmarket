4ed806e4f313b7c16911d22dc802c03ff6f9ae46
Muhammad Izzuddin al Fikri <izzuddin.alfikri@icloud.com>
2020-06-27 15:17:24 +0700

    Init. Laravel + React preset for frontend

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Laravel Telescope
+        if ($this->isLocal()) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    private function isLocal() {
+        return config('app.env') === 'local';
+    }
+}
